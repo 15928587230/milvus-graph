af8d5515498aa338f7d50948868d557b9b4bdab3
Your Name <you@example.com>
2024-07-25 01:17:11 +0800

    feat(langchain4j): GraphRAG Java实体关系提取测试，采用百度免费LLM

--- src/test/java/com/owinfo/milvus/document/LLMUtils.java
+++ src/test/java/com/owinfo/milvus/document/LLMUtils.java
@@ -4,12 +4,14 @@ import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentParser;
 import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParserFactory;
+import dev.langchain4j.data.document.splitter.DocumentByCharacterSplitter;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.qianfan.QianfanChatModel;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class LLMUtils {
@@ -33,9 +35,24 @@ public class LLMUtils {
         DocumentParser documentParser = parserFactory.create();
         ClassPathResource resource = new ClassPathResource(filePath);
         Document document = documentParser.parse(resource.getInputStream());
-        DocumentSplitter splitter = DocumentSplitters.recursive(600, 50);
+
+        replaceBlank(document, false);
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(300, 50);
         List<TextSegment> textSegments = splitter.split(document);
         // 少于120的分段文本没有参考意义
         return textSegments.stream().filter(textSegment -> textSegment.text().length() > 120).toList();
+    }
+
+    private static void replaceBlank(Document document, boolean replace) {
+        if (!replace) return;
+        try {
+            String replacedText = document.text().replaceAll("\\s*", "");
+            Field declaredField = document.getClass().getDeclaredField("text");
+            declaredField.setAccessible(true);
+            declaredField.set(document, replacedText);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
